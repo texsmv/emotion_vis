623c80022a08f8be3c1e12a8c43934fad6c69797
Alexis <texs.mv@gmail.com>
2021-06-14 13:14:13 -0500

    updates

--- ui/flutter_service_worker.js
+++ ui/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "405caa85054c42715f15f934b0d4cde8",
+  "main.dart.js": "f91a277ee4ca51df8de5fef47b1bbea7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "b173a9f5e002d3a33f772f1b66d67c02",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "be56e55548cd55b0ba11f1765059d0ab",
-"/": "be56e55548cd55b0ba11f1765059d0ab",
+"index.html": "22e5fbeaed88c4660965a2f89f95d391",
+"/": "22e5fbeaed88c4660965a2f89f95d391",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/glass_kit/assets/noise.png": "86f22ae1a498bb8f0c39264f9c7c796c",
